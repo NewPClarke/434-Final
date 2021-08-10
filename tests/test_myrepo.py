04c58bf3708e4ca2193e2325c446e9d044fdd619
NewPClarke <87274866+NewPClarke@users.noreply.github.com>
2021-08-09 21:57:53 -0500

    Update test_myrepo.py

--- tests/test_myrepo.py
+++ tests/test_myrepo.py
@@ -1,6 +1,14 @@
 from myrepolib import repomod
-
+import main
 
 def test_func():
-    result = repomod.myfunc()
-    assert result == 1
+	result = repomod.myfunc()
+	assert result == 1
+
+def test_index():
+    main.app.testing = True
+    client = main.app.test_client()
+
+    r = client.get("/")
+    assert r.status_code == 200
+    assert "Hello World!" in r.data.decode("utf-8")
